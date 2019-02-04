18b6a0ece615d6c722a95856359cca963806c41b
WhiteSpiderLily <44296243+WhiteSpiderLily@users.noreply.github.com>
2019-02-04 14:19:58 -0500

    Add files via upload

---
+++ test_sample.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  4 12:53:02 2019
+
+@author: Siyuan
+"""
+
+# content of test_sample.py
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(3) == 4
